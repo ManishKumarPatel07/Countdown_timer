9529d6cd1e9143d56412f9a69b4ee7d5e5d9f5a4
ManishKumarPatel07 <133961893+ManishKumarPatel07@users.noreply.github.com>
2023-05-28 13:52:32 +0530

    Add files via upload
    1st commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const countdown = () => {
+    const currentDate = new Date();
+    const eventDate = new Date('2024-01-01');
+  
+    const totalSeconds = (eventDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+  };
+  
+  countdown();
+  
+  setInterval(countdown, 1000);
